d53be13906c78f3b45c9f3a843454e93d4813479
TakahiroIshii <itkh@amazon.com>
2021-05-18 15:41:51 +0900

    feat: add files for initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # demo-fluent-kinesis
+
+## packages
+```
+yarn add -D @types/node fluent-logger nodemon prettier ts-node typescript
+yarn add aigle
+```
+
+## fluentd agent
+fluentd agent for Amazon Linux
+### td-agent 4
+```
+curl -L https://toolbelt.treasuredata.com/sh/install-amazon2-td-agent4.sh | sh
+```
+### td-agent 3
+```
+curl -L https://toolbelt.treasuredata.com/sh/install-amazon2-td-agent3.sh | sh
+```
+
+## get fluent-plugin-kinesis
+```
+td-agent-gem install fluent-plugin-kinesis
+```
+
+## run fluentd agent
+```
+sudo td-agent -c /etc/td-agent/td-agent.conf
+```
+
+## edit fluentd agent config
+```
+sudo vim  /etc/td-agent/td-agent.conf
+```
+
---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import Aigle from "aigle";
+const logger = require("fluent-logger");
+
+logger.configure("demo-fluent-kinesis", {
+  host: "localhost",
+  port: 24226,
+  timeout: 3.0,
+  reconnectInterval: 600000,
+});
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const main = async () => {
+  const myArgs = process.argv.slice(2);
+  const num = Number(myArgs[0]);
+  await Aigle.timesSeries(num, async () => {
+    await sleep(2000);
+    const date = new Date();
+    const record = `${date.toLocaleString()}, system log test`;
+    console.log(record);
+    logger.emit("label", { record });
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
